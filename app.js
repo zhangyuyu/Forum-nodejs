c5bad4cc8cebc7d3901579b5b718d46ecf64b649
yuzhang <1580074674@qq.com>
2016-07-17 23:32:22 +0800

    Add not found router

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use('/forum-api', routers);
 
+app.get('*', (req, res) => {
+  res.status(404)
+     .send('NOT FOUND!');
+});
+
 app.listen(3000, () => {
   console.log('Forum app listening on port 3000!');
 });
